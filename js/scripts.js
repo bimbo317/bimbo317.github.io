16a81f3587584efb3740b6374304c830840ea591
Bimbo317 <bimpo2002@hotmail.com>
2023-03-01 03:34:29 -0300

    Eliminando carpeta docs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /* funcion para leer los datos desde randomuser*/
 async function getuser() {
-    const person = await fetch('../data.json')
+    const person = await fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then(data => data.results[0])
     checkDataPerson(person);
